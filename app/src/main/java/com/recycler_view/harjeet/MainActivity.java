ad6a44ad3760468e8fa621b2002effc2f982ebb9
Your Name <harjeet17@navgurukul.org>
2019-01-01 16:40:17 +0530

    MunshiG Login

---
+++ app/src/main/java/com/recycler_view/harjeet/MainActivity.java
@@ -0,0 +1,41 @@
+package com.recycler_view.harjeet;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.support.v7.widget.Toolbar;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Account Setting");
+        button=findViewById(R.id.sendBtn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
+
